a6b2db1e4402a4dc7bfe53ee7ea9e3382b63b3a0
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2021-12-26 20:08:04 +0300

    CREATE MyFrame class

---
+++ src/MainPackege/MyFrame.java
@@ -0,0 +1,17 @@
+package MainPackege;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 800;
+
+    MyFrame(){
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setMenuBar(new MyMenuBar());
+
+        setVisible(true);
+    }
+}
